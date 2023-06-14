c338f31c1bf7f3f0ee47e3b0be5a0d9785cdd5f3
Florian ARGAUD <argaudflorian@gmail.com>
2023-06-14 14:00:45 +0200

    feat: import dirs

--- playground/app.vue
+++ playground/app.vue
@@ -1,8 +1,7 @@
 <template>
   <div>
-    Nuxt module playground!
+    <t-large-button>Hello world</t-large-button>
   </div>
 </template>
 
-<script setup>
-</script>
+<script setup></script>
--- src/module.ts
+++ src/module.ts
@@ -1,19 +1,20 @@
-import { defineNuxtModule, addPlugin, createResolver } from '@nuxt/kit'
+import { defineNuxtModule } from "@nuxt/kit";
 
-// Module options TypeScript interface definition
+import { join } from "pathe";
+
 export interface ModuleOptions {}
 
 export default defineNuxtModule<ModuleOptions>({
   meta: {
-    name: 'my-module',
-    configKey: 'myModule'
+    name: "@tailplate/ui",
+    configKey: "tailplate",
   },
-  // Default configuration options of the Nuxt module
-  defaults: {},
-  setup (options, nuxt) {
-    const resolver = createResolver(import.meta.url)
-
-    // Do not add the extension since the `.ts` will be transpiled to `.mjs` after `npm run prepack`
-    addPlugin(resolver.resolve('./runtime/plugin'))
-  }
-})
+  hooks: {
+    "components:dirs": (dirs) => {
+      dirs.push({
+        path: join(__dirname, "runtime/components"),
+        prefix: "t",
+      });
+    },
+  },
+});
